5c38caa00e8dff92443bb01b94647517541ee880
Austin <austin_h_s@hotmail.com>
2026-01-13 23:48:15 -0600

    remove faucet now that it doesnt exist

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './classes/ProtocolReader';
-export * from './classes/Faucet';
 export * from './classes/OracleManager';
 export * from './classes/Market';
 export * from './classes/CToken';
