0e89da122aaf9a9c63d744b1cf89a45d1fdf5581
Austinhs <austin_h_s@hotmail.com>
2026-02-17 00:07:19 -0500

    update v3.4.6 to hide YZM market

--- src/classes/Market.ts
+++ src/classes/Market.ts
@@ -693,6 +693,11 @@ export class Market {
             const userData    = all_data.userData.markets[i]!;
 
             const market_address = staticData.address;
+
+            if(market_address == '0x4409e2161B6f9A25ACbC471a37390654Ed272249') {
+                continue;
+            }
+
             let deploy_data: DeployData | undefined;
 
             for(const obj_key of deploy_keys) {
